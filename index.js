46c7068397e9512c1db1193671ae0628bc092bd9
Wes Mason <wes@1stvamp.org>
2024-11-23 03:55:29 +0000

    Working process with tailscale automation

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+#!/usr/bin/env node
 
-var proxy = httpProxy.createProxyServer({});
+const http = require('http');
+const httpProxy = require('http-proxy');
+const portfinder = require('portfinder');
+const { spawn } = require('node:child_process');
 
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('Host', 'foobar');
+const proxy = httpProxy.createProxyServer({});
+
+if (process.argv[2] == '-h' || process.argv[2] == '--help') {
+  console.log(`USAGE: @1stvamp/proxy TARGET_URL
+TARGET_URL like a local client site, e.g.: http://acme.localhost.me:3001
+
+-h, --help    This help message.`);
+  process.exit(0);
+}
+
+const target = process.argv[2];
+const domain = (new URL(target)).hostname;
+
+proxy.on('proxyReq', (proxyReq, req, res, options) => {
+  proxyReq.setHeader('Host', domain);
+});
+proxy.on('error', (err) => {
+  console.error(err);
 });
 
-var server = http.createServer(function(req, res) {
-  proxy.web(req, res, {
-    target: 'http://127.0.0.1:5050'
+portfinder.getPort((err, port) => {
+  const server = http.createServer((req, res) => {
+    proxy.web(req, res, {
+      target: target
+    });
+  });
+
+  server.listen(port);
+
+  const ts = spawn('tailscale', ['serve', port.toString()]);
+  ts.stdout.on('data', (data) => {
+    console.log(`[tailscale] ${data}`);
+  });
+  ts.stderr.on('data', (data) => {
+    console.error(`[tailscale] ${data}`);
+  });
+
+  const afton = () => {
+    ts.kill('SIGINT');
+    process.exit()
+  };
+  process.on('exit', afton);
+  process.on('SIGINT', afton);
+  process.on('SIGTERM', afton);
+  process.on('SIGUSR1', afton);
+  process.on('SIGUSR2', afton);
+  process.on('uncaughtException', (err) => {
+    console.error(err);
+    process.exit(1)
   });
 });
-
-console.log("listening on port 5050")
-server.listen(5050);
